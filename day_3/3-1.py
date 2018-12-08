551f82ca6dac7e5cc07c31a12b3e66d23240b277
Markus Suomalainen <markus.suomalainen@gmail.com>
2018-12-08 21:56:39 +0200

    Some data refactoring stuff. Still at day3

--- day_3/3-1.py
+++ day_3/3-1.py
@@ -1,5 +1,29 @@
 from data_reader import get_data
+import numpy as nu
+
+
+def refactor_data(arr=[]):
+    refactored = []
+    for row in arr:
+        elf_id, useless, position, size = row.split()
+        top = position.split(',')[0]
+        left = position.split(',')[1]
+        width = size.split('x')[0]
+        height = size.split('x')[1]
+        area_object = {
+            elf_id[1:]: {
+                "top": top,
+                "left": left,
+                "width": width,
+                "height": height,
+            }
+        }
+        refactored.append(area_object)
+
+    return refactored
+
 
 if __name__ == '__main__':
-    data = get_data(True)
-    print(data)
+    intial_data = get_data(False)
+    data = refactor_data(intial_data)
+
