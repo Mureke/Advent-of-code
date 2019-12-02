2dd7a10b1b4f0bfe937c552a3ffdbe849562d87e
Markus Suomalainen <markus.suomalainen@gmail.com>
2019-12-02 11:27:04 +0200

    Yay

--- data_reader.py
+++
@@ -1,9 +0,0 @@
-def get_data(test=False):
-    file = 'inputs.txt'
-    if test:
-        file = 'sample.txt'
-    text_file = open(file, "r")
-    lines = text_file.read().splitlines()
-    text_file.close()
-
-    return lines
