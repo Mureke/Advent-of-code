3d4577c2f3903c489a9c456f86b1fb873dafe204
Markus Suomalainen <markus.suomalainen@gmail.com>
2019-12-03 16:55:23 +0200

    will continue later

--- 2019/day_2/target/debug/day_2.d
+++
@@ -1,1 +0,0 @@
-/home/markus/Private/git/AoC/2019/day_2/target/debug/day_2: /home/markus/Private/git/AoC/2019/day_2/src/main.rs
--- 2019/day_2/target/debug/deps/day_2-6ffe39ccca8c3fd7.d
+++
@@ -1,5 +0,0 @@
-/home/markus/Private/git/AoC/2019/day_2/target/debug/deps/day_2-6ffe39ccca8c3fd7: src/main.rs
-
-/home/markus/Private/git/AoC/2019/day_2/target/debug/deps/day_2-6ffe39ccca8c3fd7.d: src/main.rs
-
-src/main.rs:
