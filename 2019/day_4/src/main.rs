7b3b3bddb3acdf06aa72fe2b01522d195fe274b7
Mureke <markus.suomalainen@gmail.com>
2019-12-14 13:37:24 +0200

    day 4 part 1 done

--- 2019/day_4/src/main.rs
+++ 2019/day_4/src/main.rs
@@ -5,17 +5,18 @@ fn main() {
     let now = Instant::now();
 
     let mut j = 0;
-    let re1 = Regex::new(r"^(\w)\1+$").unwrap();
-    let re2 = Regex::new(r"^(?=\d{6}$)1?2?3?4?5?6?7?8?9?0?$").unwrap();
+    let mut j2 = 0;
+    let re1 = Regex::new(r"(\w)\1+").unwrap();
+    let re2 = Regex::new(r"^(?=\d{6}$)0*1*2*3*4*5*6*7*8*9*$").unwrap();
+
     for i in 402328..864247 + 1 {
         if re1.find(&i.to_string()).unwrap().is_some() && re2.find(&i.to_string()).unwrap().is_some() {
-            println!("{}", i);
             j += 1
         }
     }
     println!("1: {}", j);
+    println!("2: {}", j2);
 
     println!("Time elapsed: {}ms", now.elapsed().as_millis());
 
 }
-// < 461920
