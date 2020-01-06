9f6bc1dd991517cf44af12b4938c7fce1e7632bf
Mureke <markus.suomalainen@gmail.com>
2020-01-06 22:52:02 +0200

    Getting modes and instructions

---
+++ 2019/day_5/src/utils.rs
@@ -0,0 +1,28 @@
+pub fn number_to_vec(n: i32) -> (Vec<i32>, i32) {
+    let mut digits= Vec::new();
+    let mut n = n;
+    let mut i = 0;
+    while n > 9 {
+        digits.push(n % 10);
+        n = n / 10;
+        i += 1;
+    }
+    digits.push(n);
+
+    (digits, i)
+}
+
+pub fn right_pad_with_zeros(mut vector: Vec<i32>, size: i32) -> Vec<i32> {
+    for i in 0..size {
+        vector.push(0)
+    }
+    vector
+}
+
+pub fn left_pad_with_zeros(mut vector: Vec<i32>, size: i32) -> Vec<i32> {
+    for i in 0..size {
+        vector.push(0)
+    }
+    vector.reverse();
+    vector
+}
