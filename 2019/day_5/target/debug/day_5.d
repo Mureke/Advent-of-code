088355c5ae9bd0ba48f7bc7b256ec57b0b001cb4
Mureke <markus.suomalainen@gmail.com>
2020-01-06 21:54:24 +0200

    Just testing some rust vector stuff

--- 2019/day_5/target/debug/day_5.d
+++ 2019/day_5/target/debug/day_5.d
@@ -1,1 +1,1 @@
-/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/day_5: /home/mureke/Private/git/Advent-of-code/2019/day_5/src/main.rs
+/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/day_5: /home/mureke/Private/git/Advent-of-code/2019/day_5/src/main.rs /home/mureke/Private/git/Advent-of-code/2019/day_5/src/utils.rs
--- 2019/day_5/target/debug/deps/day_5-1eef706f818e28b2.d
+++ 2019/day_5/target/debug/deps/day_5-1eef706f818e28b2.d
@@ -1,5 +1,6 @@
-/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/deps/day_5-1eef706f818e28b2: src/main.rs
+/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/deps/day_5-1eef706f818e28b2: src/main.rs src/utils.rs
 
-/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/deps/day_5-1eef706f818e28b2.d: src/main.rs
+/home/mureke/Private/git/Advent-of-code/2019/day_5/target/debug/deps/day_5-1eef706f818e28b2.d: src/main.rs src/utils.rs
 
 src/main.rs:
+src/utils.rs:
