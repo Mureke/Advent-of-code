4f0b6e076b5b7333dbe387ef4d5f0241b661cb46
Markus Suomalainen <markus.suomalainen@gmail.com>
2020-12-01 14:00:19 +0200

    Small fix to day1

--- 2020/day_1/day1.py
+++ 2020/day_1/day1.py
@@ -3,10 +3,18 @@
 if __name__ == '__main__':
     with open('data.txt', 'r') as f:
         lines = list(map(int, f.readlines()))
-
+        a1 = None
+        a2 = None
         for i in lines:
-            if (2020 - i) in lines:
-                print('Answer to 1: ', i * (2020 - i))
+            if not a1 and (2020 - i) in lines:
+                a1 = i * (2020 - i)
             for j in lines:
-                if (2020 - i - j) in lines:
-                    print('Answer to 2: ', i * j * (2020-i-j))
+                if not a2 and (2020 - i - j) in lines:
+                    a2 = i * j * (2020-i-j)
+            if a1 and a2:
+                break
+
+        print('Answer to 1: ', a1)
+        print('Answer to 2: ', a2)
+
+
