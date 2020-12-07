ff07f978156412b3f4b0ee09b549eaa74122f582
Markus Suomalainen <markus.suomalainen@gmail.com>
2020-12-07 10:08:28 +0200

    Day5 part 1

---
+++ 2020/day_5/day5_1.py
@@ -0,0 +1,37 @@
+ROWS_START = 0
+ROWS_END = 127
+COLS_START = 0
+COLS_END = 7
+
+
+def decode_line(line='FBFBBFFRLR'):
+    row_start = ROWS_START
+    row_end = ROWS_END
+    cols_start = COLS_START
+    cols_end = COLS_END
+    for i in line:
+        if i == 'F':
+            row_end -= int((row_end - row_start) / 2) + 1
+        elif i == 'B':
+            row_start = int((row_end + row_start) / 2) + 1
+        elif i == 'L':
+            cols_end -= int((cols_end - cols_start) / 2) + 1
+        elif i == 'R':
+            cols_start = int((cols_end + cols_start) / 2) + 1
+
+    if cols_start == cols_end and row_end == row_end:
+        return row_start * 8 + cols_start
+    raise Exception('Calc error')
+
+
+if __name__ == '__main__':
+    answer = 0
+    test = decode_line()
+    with open('data.txt', 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            temp_ans = decode_line(line)
+            if temp_ans > answer:
+                answer = temp_ans
+
+        print(answer)
