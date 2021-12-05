363056127ce33ba60559208b28171f540514e518
Markus Suomalainen <markus.suomalainen@gmail.com>
2021-12-05 18:02:41 +0200

    Day 3 part 2. Really bad soltuion but at least it works...

--- 2021/rAoC/src/lib/day3.rs
+++ 2021/rAoC/src/lib/day3.rs
@@ -11,10 +11,10 @@ fn get_parsed_data() -> Vec<Vec<u32>> {
     return matrix;
 }
 
-pub fn solve() {
+pub fn solve1() -> i32 {
     let mut matrix = get_parsed_data();
 
-    let matrix_processed : Vec<Vec<u32>> = (0..matrix[0].len()).map(|i| matrix
+    let matrix_processed: Vec<Vec<u32>> = (0..matrix[0].len()).map(|i| matrix
         .iter()
         .map(|c| c[i])
         .collect()
@@ -23,19 +23,66 @@ pub fn solve() {
     let mut gamma_rate = String::from("");
     let mut epsilon_rate = String::from("");
 
+
+    let mut i = 0;
     for list in matrix_processed {
         let one_count = list.iter().filter(|x| x == &&1).count();
         if one_count > list.len() / 2 {
             gamma_rate += "1";
             epsilon_rate += "0";
-            continue
+        } else {
+            gamma_rate += "0";
+            epsilon_rate += "1";
         }
-        gamma_rate += "0";
-        epsilon_rate += "1";
+        i += 1;
     }
+
 
     let gammai32 = i32::from_str_radix(&gamma_rate, 2).unwrap();
     let epsiloni32 = i32::from_str_radix(&epsilon_rate, 2).unwrap();
 
-    println!("Day3:\n1. {}",  gammai32*epsiloni32);
+    gammai32 * epsiloni32
+}
+
+fn solve2() -> i32 {
+    let mut matrix = get_parsed_data();
+
+    let i = 0;
+    let mut oxygen_generator_rating = matrix.clone();
+    let mut co2_scrubber_rating = matrix.clone();
+
+    let mut i = 0;
+
+    // NOTE: This is super ugly and repetitive code but I'm too lazy to do anything about it right now 🤷
+    while oxygen_generator_rating.len() > 1 {
+        let one_count = &oxygen_generator_rating.iter().filter(|x| x[i] == 1).count();
+        let zero_count = &oxygen_generator_rating.iter().filter(|x| x[i] == 0).count();
+        if one_count >= zero_count {
+            oxygen_generator_rating = oxygen_generator_rating.into_iter().filter(|x| x[i] != 0).collect();
+        } else {
+            oxygen_generator_rating = oxygen_generator_rating.into_iter().filter(|x| x[i] == 0).collect();
+        }
+        i += 1;
+    }
+    let mut i = 0;
+    while co2_scrubber_rating.len() > 1 {
+        let one_count = &co2_scrubber_rating.iter().filter(|x| x[i] == 1).count();
+        let zero_count = &co2_scrubber_rating.iter().filter(|x| x[i] == 0).count();
+        if one_count >= zero_count {
+            co2_scrubber_rating = co2_scrubber_rating.into_iter().filter(|x| x[i] != 1).collect();
+        } else {
+            co2_scrubber_rating = co2_scrubber_rating.into_iter().filter(|x| x[i] == 1).collect();
+        }
+        i += 1;
+    }
+    let ox_str: String = oxygen_generator_rating[0].iter().map( |x| (x).to_string()).collect();
+    let co_2_str: String = co2_scrubber_rating[0].iter().map( |x| (x).to_string()).collect();
+    i32::from_str_radix(&ox_str, 2).unwrap() * i32::from_str_radix(&co_2_str, 2).unwrap()
+}
+
+pub fn solve() {
+    let solution1 = solve1();
+    let solution2 = solve2();
+
+    println!("Day3:\n1. {}\n2. {}", solution1, solution2);
 }
