22049fc7ca7cc08902c02d4389496b46b3bd5f1b
Markus Suomalainen <markus.suomalainen@gmail.com>
2021-12-15 15:32:44 +0200

    Day 10 part 2

--- 2021/pyAoC/solutions/day_10.py
+++ 2021/pyAoC/solutions/day_10.py
@@ -18,14 +18,24 @@ def solve():
     data = data.split("\n")
 
     closing = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    closing_points_d2 = {")": 1, "]": 2, "}": 3, ">": 4}
+    pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+
     result = 0
+    result2 = []
     for item in data:
+        temp_res2 = 0
         is_valid, left_overs = check(item)
         if not is_valid and any(x in left_overs for x in closing.keys()):
             for i in left_overs:
                 if i in closing.keys():
-                    print(closing[i])
                     result += closing[i]
                     break
 
-    print(f"Day10: 1: {result}")
+        if not is_valid and all(x in pairs.keys() for x in left_overs):
+            for i in left_overs[::-1]:
+                temp_res2 = temp_res2 * 5 + closing_points_d2[pairs[i]]
+        if temp_res2 != 0:
+            result2.append(temp_res2)
+    result2.sort()
+    print(f"Day10: 1: {result} 2. {result2[int(len(result2) / 2)]}")
